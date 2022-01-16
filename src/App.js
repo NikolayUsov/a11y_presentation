d9fd01987114c93cf55b03acc51b170762d1aef2
Nikolay Usov <uskovnikolaj@MacBook-Pro-Pincuk.local>
2022-01-16 18:51:31 +0300

    add_virst_good_version

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import OrderForm from './components/OrderForm';
 function App() {
   return (
     <div className="App">
+      <a class="skip-main" href="#main">Пропустить навигацию</a>
       <Header />
-      <div role="main" aria-labelledby="title1">
+      <div role="main" aria-labelledby="title1" id='main'>
 
           <h1 id="title1" className='visuallyHidden'>Основная страница магазина</h1>
         <Promo />
